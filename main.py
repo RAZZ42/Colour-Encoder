0f9f047f1ac9deaf12e5d52c4070b7cada938687
Neophyt3-sudo <ruslan.bone@outlook.com>
2021-08-14 21:44:48 +0100

    Fixed '#' problem and added comments to the script.

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 # Target values
 BLUE = list("0000ff")
 GREEN = list("00ff00")
-GENERIC = 0x000000
+GENERIC = str("0x000000")
 
-# install libraries
-#import numpy as np
-
-# Encoder
+# User input
 input_encoder = input("Input: ").lower()
 
+# Converts into a list.
 lst_input_encoder = list(input_encoder)
 
+# defines both two functions to discriminate whether it is a blue or green hexadecimal, else it is confused.
 def not_blue():
+    encoded_lst = []
     if lst_input_encoder[-2:-1] >= BLUE[-2:-1]:  # This only works with two last FFs
-
-        for n in lst_input_encoder:
-            if n[0] != GENERIC[0]:
-                hex_num = "#" + str(lst_input_encoder[0:])
-                hex_joined = "".join(hex_num)
-        print(hex_joined)
-
-        print("Blue")
+        if lst_input_encoder[0] != GENERIC[1]:  # This checks to confirm if # exists - if not, it creates into encoder.
+            lst_input_encoder[:0] = ["0x"]  # Places '0x' at the start of the string.
+            hex_num = "".join(lst_input_encoder)  # Joins the string together.
+            encoded_lst = hex_num  # Converts into hexadecimal by going into a list.
+            print(encoded_lst)  # Checks to see if it works or not.
+        print("Blue")  # Prints the outcome.
     else:
-        print("Not Blue")
+        print("Not Blue")  # Self-explanatory.
+
+# Description above applies to the def below. They are both the same.
 
 def not_green():
+    encoded_lst = []
     if lst_input_encoder[-4:-2] >= GREEN[-4:-2]:  # This only works with two middle FFs
+        if lst_input_encoder[0] != GENERIC[1]:
+            lst_input_encoder[:0] = ["0x"]
+            hex_num = "".join(lst_input_encoder)
+            encoded_lst = hex_num
+            print(encoded_lst)
         print("Green")
     else:
         print("Not Green")
@@ -33,8 +39,18 @@ def not_green():
 
 not_blue()
 not_green()
-#----------------------------
-#  Quick to do list:
-#  Explain each line of code.
-#  Fix up the #.
-#-----------------------------
+
+
+
+#---------------------------------
+#       Quick to do list:
+# 1 Learn this fancy code:
+#       i = 0xhelloworld
+#       hex_string = f'0x{i:>010X}'
+#       print(hex_string)
+# 2 Scrape data off web and test
+#   the script out.
+#
+#
+#
+#---------------------------------
